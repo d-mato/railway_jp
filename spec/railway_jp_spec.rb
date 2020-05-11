2a76d01bcb843b8a9a282ef5132649bfa75079be
Daiki Matoba <telnetstat@gmail.com>
2020-05-11 20:54:25 +0900

    Set up CI

--- spec/railway_jp_spec.rb
+++ spec/railway_jp_spec.rb
@@ -1,9 +1,5 @@
 RSpec.describe RailwayJp do
-  it "has a version number" do
+  it 'has a version number' do
     expect(RailwayJp::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
